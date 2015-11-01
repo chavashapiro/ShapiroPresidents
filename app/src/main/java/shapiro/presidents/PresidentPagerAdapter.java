3003a55864d71042ddf92c0dc6c8fb475a2000da
unknown <chavs915@gmail.com>
2015-11-01 11:17:33 -0500

    added all president data

--- app/src/main/java/shapiro/presidents/PresidentPagerAdapter.java
+++ app/src/main/java/shapiro/presidents/PresidentPagerAdapter.java
@@ -32,10 +32,24 @@ public class PresidentPagerAdapter extends PagerAdapter {
         //findViewById doesn't work by itself here b/c it's not part of PageAdapte
         TextView name = (TextView) view.findViewById(R.id.name);
         TextView number = (TextView) view.findViewById(R.id.number);
+        TextView aliveYears = (TextView) view.findViewById(R.id.aliveYears);
+        TextView officeYears = (TextView) view.findViewById(R.id.officeYears);
+        TextView party = (TextView) view.findViewById(R.id.party);
 
         President president = presidents.get(position);
         name.setText(president.getPresident());
         number.setText(String.valueOf(president.getNumber()));
+        if(president.getDeathYear() != null) {
+            aliveYears.setText(String.valueOf(president.getBirthYear() + "-" + String.valueOf(president.getDeathYear())));
+        } else {
+            aliveYears.setText(String.valueOf(president.getBirthYear()));
+        }
+        if (president.getLeftOffice() != null) {
+            officeYears.setText(president.getTookOffice() + "--" + president.getLeftOffice());
+        } else {
+            officeYears.setText(president.getTookOffice());
+        }
+        party.setText(president.getParty());
 
         container.addView(view);
         return view;
